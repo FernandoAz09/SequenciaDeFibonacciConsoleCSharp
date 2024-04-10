275df97f2f220f4f2e738d60cf3cc7be1d8e1b20
Fernando Azevedo <fernando_litoral@hotmail.com>
2024-04-10 07:38:24 -0300

    Sequencia de Fibonacci em CShap

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var fibonacci = new List<int> { 1, 1 };
+
+
+        while (fibonacci.Count <= 30)
+        {
+            var previousNumber1 = fibonacci[fibonacci.Count - 1];
+            var previousNumber2 = fibonacci[fibonacci.Count - 2];
+
+            var result = previousNumber1 + previousNumber2;
+            fibonacci.Add(result); 
+            Console.WriteLine(result);
+        }
     }
 }
